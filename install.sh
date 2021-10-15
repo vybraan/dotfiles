268ab90874ea49fe1db6bea923d6ef0d3d00e079
Ailton Bauque <neray@alnky.localdomain>
2021-10-15 15:33:47 -0500

    updating the script

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+mv ~/.config/i3 ~/.config/i3.preVybraan
+cp -rf ./i3 ~/.config/
+
+cp -rf ./pics/* ~/Pictures
+
+mv ~/config/polybar/ ~/.config/polybar.preVybraan
+cp -rf ./polybar ~/.config/
